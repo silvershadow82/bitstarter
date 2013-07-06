02df556e2118e23477cd63df6101a818f760f329
Ruslan Shtyk <cjshadow@gmail.com>
2013-07-06 16:10:26 +0000

    Updated web.js to read string from file 2

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var contents =   fs.readFileSync('index.html');
-    response.send(contents);
+    var contents = fs.readFileSync('index.html');
+    var buf = new Buffer(contents);
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
