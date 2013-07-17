7592334695782ad97760af9f78ebfec26c2ecfff
Ruslan Shtyk <cjshadow@gmail.com>
2013-07-17 19:36:47 +0000

    Styled version of the index.html

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var fs = require('fs'); 
 var app = express.createServer(express.logger());
@@ -8,7 +10,7 @@ app.get('/', function(request, response) {
     response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
